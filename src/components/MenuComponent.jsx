be1704ba717069cf2d8801cb786f0e8e736ad222
Mamun <mamun.ruet.ete@gmail.com>
2019-04-07 13:08:50 +0200

    event handler in Menu

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -6,19 +6,40 @@ class Menu extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+            selectedDish: null,
         };
     }
 
+    onDishSelect = dish => {
+      this.setState({selectedDish: dish});
+    };
+
+
+    renderDish = dish=> {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
 
     render() {
         const menu = this.props.dishes.map(dish => {
             return(
-                <div  className="col-12 col-md-5 m-1">
-                    <Card key={dish.id}>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={()=> this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
+                            <CardTitle><h3>{dish.name}</h3></CardTitle>
                         </CardImgOverlay>
                     </Card>
                 </div>
@@ -28,6 +49,12 @@ class Menu extends Component{
             <div className='container'>
                 <div className='row'>
                     {menu}
+                </div>
+
+                <div className="row">
+                    <div  className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.state.selectedDish)}
+                    </div>
                 </div>
             </div>
         );
