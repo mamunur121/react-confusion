2ecf7fafaa8640ece849fc1d617ed88218709dfd
Mamun <mamun.ruet.ete@gmail.com>
2019-04-07 12:52:31 +0200

    render using Card than List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import {DISHES} from "./shared/dishes";
 import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
   render() {
-
     return (
       <div className="App">
         <Navbar color='light'>
@@ -13,7 +19,7 @@ class App extends Component {
             <NavbarBrand href='/'> Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-          <Menu />
+          <Menu dishes={this.state.dishes} />
       </div>
     );
   }
