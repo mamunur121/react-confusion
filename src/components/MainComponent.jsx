05c7219700da96a53002b7d314669baa3c1fb73d
Mamun <mamun.ruet.ete@gmail.com>
2019-04-13 16:18:37 +0200

    single page application part 2

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -32,14 +32,28 @@ class Main extends Component {
                     leader={this.state.leaders.filter(leader => leader.featured)[0]}
                 />
             );
-        }
+        };
+
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetails
+                    dish={this.state.dishes.filter((dish) =>
+                        dish.id === parseInt(match.params.dishId,10))[0]}
+
+                    comments={this.state.comments.filter((comment) =>
+                        comment.dishId === parseInt(match.params.dishId,10))} />
+            );
+        };
+
         return (
             <div className="App">
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage}/>
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes}/>}/>
                     <Route exact path='/contactus' component={Contact} />
+
                     <Redirect to='/home'/>
                 </Switch>
 
